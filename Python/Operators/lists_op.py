01b76f3ed3d0bce3a5f7ac8efda1095c3e93381a
reruu <jarellabella1000@gmail.com>
2026-07-09 11:46:03 +0800

    new changes

---
+++ Python/Operators/lists_op.py
@@ -0,0 +1,10 @@
+even_numbers = [2,4,6,8]
+odd_numbers = [1,3,5,7]
+all_numbers = odd_numbers + even_numbers
+print(all_numbers)
+
+# [1, 3, 5, 7, 2, 4, 6, 8]
+
+print([1,2,3] * 3)
+
+#print([1,2,3] * 3)
--- Python/Operators/operators.py
+++ Python/Operators/operators.py
@@ -19,10 +19,17 @@ hello = "hello"
 
 print(one + two + hello)
 '''
-squared = 7 ** 2
-cubed = 2 ** 3
+squared = 7 ** 2 #49
+cubed = 2 ** 3 #8
 print(squared)
 print(cubed)
+
+lotsofhellos = "hello" * 10
+print(lotsofhellos) 
+
+#hellohellohellohellohellohellohellohellohellohello
+
+
 
 
 
